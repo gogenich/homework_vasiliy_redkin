860901cfb40f61fad42ac7b8d863540eb43955d1
vasia <vas.redckin1993@gmail.com>
2021-02-14 22:54:02 +0300

    выполнил 6 задание третьего урока

---
+++ lesson_3/the_task_6.py
@@ -0,0 +1,10 @@
+def func(world):
+    print(world.title())
+
+world = input('Введите слово с маленькой буквы: ')
+func(world)
+
+world = input('Введите слова с маленькой буквы через пробел: ')
+func(world)
+
+
