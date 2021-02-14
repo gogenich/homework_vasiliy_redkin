2d5557270cab9315c8f870986f52dc6296b2fe41
vasia <vas.redckin1993@gmail.com>
2021-02-14 22:46:24 +0300

    встроил функцию пятое задание, урок 3

--- lesson_3/the_task_5.py
+++ lesson_3/the_task_5.py
@@ -1,12 +1,18 @@
-
-sum = 0
-i = True
-while i == True:
-    nambers = (input('введите числа через пробел либо @ для выхода из функции: '))
-    nambers = nambers.split()
+def my_func(nambers):
+    """считаем сумму введенных чисел, а так же проверяем на наличие символа для выхода"""
+    sum = 0
     for namber in nambers:
         if namber == "@":
-            i = False
+            global I
+            I = False
         else:
             sum = sum + int(namber)
-    print(sum)
+    return sum
+
+I = True
+while I == True:
+    nambers = (input('введите числа через пробел либо @ для выхода из функции: '))
+    nambers = nambers.split()
+    print(f'сумма введенных чисел равна: {my_func(nambers)}')
+
+
